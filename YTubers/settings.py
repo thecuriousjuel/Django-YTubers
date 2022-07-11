d33bda1f61e78604efa3bc19bd3e3dd8fad1fdbd
Biswajit Basak <juelbasak@gmail.com>
2022-07-12 02:40:04 +0530

    Fixing bugs in settings file

--- YTubers/settings.py
+++ YTubers/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'YTubers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'YTubers',
+        'USER': 'postgres',
+        'PASSWORD': '1212',
+        'HOST': 'localhost'
     }
 }
 
